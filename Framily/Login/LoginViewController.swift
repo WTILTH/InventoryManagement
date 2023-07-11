2fa0245d713e9ba3252833ff330f3a10b61f10ab
SKVarun <skvarun@wtilth.com>
2023-07-11 17:36:39 +0530

    11/07/2023 5:36

--- Framily/Login/LoginViewController.swift
+++ Framily/Login/LoginViewController.swift
@@ -12,52 +12,87 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var emailIdText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    var iconClick = false
+    let imageIcon = UIImageView()
     let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
         printSavedData()
+        imageIcon.image = UIImage(named: "closeEye")
+                let contentView = UIView()
+                contentView.addSubview(imageIcon)
+                
+                contentView.frame = CGRect(x: 0, y: 0, width: UIImage(named: "closeEye")!.size.width, height: UIImage(named: "closeEye")!.size.width)
+                
+                imageIcon.frame = CGRect(x: -10, y: 0, width: UIImage(named: "closeEye")!.size.width, height: UIImage(named: "closeEye")!.size.width)
+                passwordText.rightView = contentView
+                passwordText.rightViewMode = .always
+                
+                let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+                imageIcon.isUserInteractionEnabled = true
+                imageIcon.addGestureRecognizer(tapGestureRecognizer)
+                
+            }
+            
+            
+            @objc func imageTapped(tapGestureRecognizer:UITapGestureRecognizer)
+    {
+        
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        if iconClick
+        {
+            iconClick = false
+            tappedImage.image = UIImage(named: "openEye")
+            passwordText.isSecureTextEntry = false
+        }
+        
+        else {
+            
+            iconClick = true
+            tappedImage.image = UIImage(named: "closeEye")
+            passwordText.isSecureTextEntry = true
+            
+            
+        }
     }
-
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
 
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         
-        guard let emailID = emailIdText.text, !emailID.isEmpty,
-            let password = passwordText.text, !password.isEmpty else {
-                
-                showCustomAlertWith(message: "Fill all the fields", descMsg: "Please enter both email and password.")
-                return
-        }
-
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
-        fetchRequest.predicate = NSPredicate(format: "emailID == %@", emailID)
-
-        do {
-            let result = try managedContext.fetch(fetchRequest)
-            if let user = result.first as? User {
-                if user.password == password {
-                    
-                    performSegue(withIdentifier: "loginToOtp", sender: nil)
-                } else {
-                    
-                    showCustomAlertWith(message: "Incorrect password", descMsg: "Incorrect password.")
+        guard let loginInput = emailIdText.text, !loginInput.isEmpty,
+                    let password = passwordText.text, !password.isEmpty else {
+                        showCustomAlertWith(message: "Please enter both login and password", descMsg: "Please enter both login and password.")
+                        return
                 }
-            } else {
-                
-                showCustomAlertWith(message: "user found with the provided email", descMsg: "No user found with the provided email.")
+  
+                guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                    return
+                }
+                let managedContext = appDelegate.persistentContainer.viewContext
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+                fetchRequest.predicate = NSPredicate(format: "password == %@", password)
+
+                do {
+                    let result = try managedContext.fetch(fetchRequest)
+                    let filteredUsers = result.compactMap { $0 as? User }.filter {
+                        $0.userName == loginInput || $0.phoneNumber == loginInput
+                    }
+                    
+                    if let user = filteredUsers.first {
+                       
+                        performSegue(withIdentifier: "loginToOtp", sender: nil)
+                    } else {
+                        showCustomAlertWith(message: "Invalid login credentials", descMsg: "Invalid login credentials.")
+                    }
+                } catch {
+                   
+                    showCustomAlertWith(message: "An error occurred during login", descMsg: "An error occurred during login.")
+                }
             }
-        } catch {
-            
-            showCustomAlertWith(message: "An error occurred during login.", descMsg: "An error occurred during login.")
-        }
-    }
+    
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "loginToOtp" {
@@ -81,7 +116,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 print("Last Name: \(user.lastName ?? "")")
                 print("User Name: \(user.userName ?? "")")
                 print("Password: \(user.password ?? "")")
-                print("----------------------")
+                print("--*------*-----*-----*---")
             }
         } catch let error as NSError {
             print("Error fetching data: \(error), \(error.userInfo)")
